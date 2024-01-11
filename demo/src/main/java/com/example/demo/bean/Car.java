613fba644b378083c1680c202d52232f8a71745e
Arthur <wangyn@qeek.cc>
2024-01-11 09:20:08 +0800

    update

---
+++ demo/src/main/java/com/example/demo/bean/Car.java
@@ -0,0 +1,11 @@
+package com.example.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by arthurwang on 2024/1/11
+ */
+
+@ConfigurationProperties
+public class Car {
+}
