e3a96f7e7700ac23bd8a92d9ac11e29ad7eb49ea
Arthur <wangyn@qeek.cc>
2025-06-17 14:02:58 +0800

    update

--- customer/src/main/java/com/yn/customer/util/Factorial.java
+++ customer/src/main/java/com/yn/customer/util/Factorial.java
@@ -2,7 +2,16 @@ package com.yn.customer.util;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.nio.charset.Charset;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 public class Factorial {
@@ -17,7 +26,27 @@ public class Factorial {
         }
     }
 
+    private static DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+            .appendValue(ChronoField.YEAR)
+            .appendLiteral("/")
+            .appendValue(ChronoField.MONTH_OF_YEAR)
+            .appendLiteral("/")
+            .appendValue(ChronoField.DAY_OF_MONTH)
+            .appendLiteral(" ")
+            .appendValue(ChronoField.HOUR_OF_DAY)
+            .appendLiteral(":")
+            .appendValue(ChronoField.MINUTE_OF_HOUR)
+            .appendLiteral(":")
+            .appendValue(ChronoField.SECOND_OF_MINUTE)
+            .appendLiteral(".")
+            .appendValue(ChronoField.MILLI_OF_SECOND).toFormatter();
+
     public static void main(String[] args) {
-        log.info("charset: {}", Charset.defaultCharset());
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy我MM就是dd HH不mm一ss样SSS");
+        String dateTime = localDateTime.format(dateTimeFormatter);
+
+        log.error("dateTime:{}", dateTime);
+
     }
 }
